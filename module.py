80b5451651ffc2e0dc4ca47fd97072c52f366759
bad-company-viv <vivek@thebadcompany.co>
2026-06-03 09:54:26 +0530

    learned importing modules

---
+++ module.py
@@ -0,0 +1,17 @@
+# import my_module
+# courses=['Maths','Science','Engineering']
+# index = my_module.find_index(courses,'Maths')
+# print(index)
+# OR
+import sys
+import calendar
+import datetime
+import os
+from my_module import find_index
+import antigravity
+courses=['Maths','Science','Engineering']
+print(find_index(courses,'Maths'))
+print(sys.path)
+print(datetime.date.today())
+
+print(os.getcwd())
